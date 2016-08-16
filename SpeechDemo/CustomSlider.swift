36e56e8da8b74fb88196cd5bba003ce71809d0cc
anbumanid <newanbu22@yahoo.com>
2016-08-16 15:27:43 +0530

    test commit

---
+++ SpeechDemo/CustomSlider.swift
@@ -0,0 +1,13 @@
+//
+//  CustomSlider.swift
+//  SpeechDemo
+//
+//  Created by Anbumani on 27/07/16.
+//  Copyright © 2016 Appcoda. All rights reserved.
+//
+
+import Cocoa
+
+class CustomSlider: UISlider {
+
+}
